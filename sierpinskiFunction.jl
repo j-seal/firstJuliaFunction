ba460b587cec364c83bfa6cccf9e0f726b6d7b3f
jseal <38115468+j-seal@users.noreply.github.com>
2019-12-22 14:30:40 -0700

    Add files via upload

---
+++ sierpinskiFunction.jl
@@ -0,0 +1,25 @@
+#Introduction to the Julia Community - jseal
+
+#Prints a (delightful) Sierpinski triangle with base length n
+function printSierpinski(n)
+    x = n - 1
+    for i in x:-1:0
+        for j in 0:i
+            print(" ")
+        end
+
+        k = 0
+        while (i + k) < n
+            if((i & k) != 0)
+                print("  ")
+            else
+                print("* ")
+            end
+            k += 1
+        end
+
+        println()
+    end
+end
+
+printSierpinski(32)
